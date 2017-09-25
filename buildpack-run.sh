36f69369d8a23ef6121d94d06596f38211f372bb
ShinjiroMoriya <moriya@tam-tam.co.jp>
2017-09-25 16:32:40 +0900

    調整

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,6 @@
-DIRECTORY=/app/dict.csv
-if [ -f $DIRECTORY ]; then
-/app/.linuxbrew/Cellar/mecab/0.996/libexec/mecab/mecab-dict-index -d /app/.linuxbrew/lib/mecab/dic/ipadic -u original.dic -f utf-8 -t utf-8 dict.csv
+FILE=/app/dict.csv
+if [ -f $FILE ]; then
+    /app/.linuxbrew/Cellar/mecab/0.996/libexec/mecab/mecab-dict-index -d /app/.linuxbrew/lib/mecab/dic/ipadic -u original.dic -f utf-8 -t utf-8 dict.csv
 else
-touch original.dic
+    touch original.dic
 fi
